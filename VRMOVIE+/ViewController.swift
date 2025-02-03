e23dc1e70e5cb295a594a3bceec49a841dbc181c
Mamed-MH <muhammedmz0619@gmail.com>
2025-02-03 10:43:25 +0400

    Initial Commit

---
+++ VRMOVIE+/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VRMOVIE+
+//
+//  Created by Mamed Hacıyev on 3.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
